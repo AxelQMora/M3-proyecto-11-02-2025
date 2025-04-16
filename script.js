b915cde0e8e685e5329a31e20a38c391db2c70ab
Axel Quintana Mora <axel.quintana.mora@outlook.com>
2025-04-16 15:10:26 -0600

    Contador con for y más frutas en el arreglo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let index = 0;
 let index2 = 0;
+let index4 = 0;
 
 const frutas = [
     { nombre: "Manzana", esCitrica: false },
@@ -13,10 +14,20 @@ const frutas = [
     { nombre: "Uva", esCitrica: false },
     { nombre: "Naranja", esCitrica: true },
     { nombre: "Fresa", esCitrica: false },
-    { nombre: "Limón", esCitrica: true }
+    { nombre: "Limón", esCitrica: true },
+    { nombre: "Piña", esCitrica: false },
+    { nombre: "Papaya", esCitrica: false },
+    { nombre: "Mandarina", esCitrica: true },
+    { nombre: "Mango", esCitrica: false },
+    { nombre: "Granada", esCitrica: false },
+    { nombre: "Cereza", esCitrica: false },
+    { nombre: "Kiwi", esCitrica: true },
+    { nombre: "Melón", esCitrica: false },
+    { nombre: "Coco", esCitrica: false },
+    { nombre: "Frambuesa", esCitrica: false }
   ];
 
-  // Contador
+  // Contador while
 total = frutas.length;
 console.log("Total de frutas: ", total);
 
@@ -28,4 +39,15 @@ while (index <= frutas.length-1) {
     index++;   
 }
 
-console.log("Número de frutas cítricas: ", index2);
+console.log("Número de frutas cítricas (while): ", index2);
+
+
+//Contador For
+
+for (let index3 = 0; index3 <= frutas.length-1; index3++) {
+    if (frutas[index3].esCitrica === true) {
+        index4++
+    }
+}
+
+console.log("Número de frutas cítricas (for): ", index4);
